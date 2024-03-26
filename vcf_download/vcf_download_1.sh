4f76a0fadec2fbeaa7fa393428ed9ef0857a709b
Till Ohlendorf <till.ohlendorf@tum.de>
2024-03-26 14:46:36 +0100

    added debugging features

--- vcf_download/vcf_download_1.sh
+++ vcf_download/vcf_download_1.sh
@@ -29,7 +29,7 @@ cd ~/vo_agam_release/v3/metadata/general/$sample_set/
 
 # Check if the metadata CSV exists and then proceed
 csv_file="samples.meta.csv"
-if [[ -f "$csv_file" ]]; then
+if [[ -f "$csv_file" ]] && [[ $(wc -l <"$csv_file") -gt 1 ]]; then
     # Get the first eleven lines of the metadata CSV file and save it temporarily
     head -n 11 "$csv_file" > temp_head.csv
 
@@ -40,16 +40,17 @@ if [[ -f "$csv_file" ]]; then
     tail -n +2 temp_head.csv | while IFS=, read -r sample_id remainder; do
     
         # Use wget to download the .vcf.gz files for each sample_id and save them in the vcf_files directory
-        wget --no-clobber -P $HOME/vcf_files "https://vo_agam_output.cog.sanger.ac.uk/$sample_id.vcf.gz"
+        wget --no-clobber -P $HOME/vcf_files "https://vo_agam_output.cog.sanger.ac.uk/$sample_id.vcf.gz" || { echo "Failed to download $sample_id.vcf.gz"; exit 1; }
         
         # Filter for chromosome 3 and save the output to a new file
-        bcftools view -r 3 $HOME/vcf_files/$sample_id.vcf.gz -o $HOME/vcf_files/${sample_id}_chr3.vcf.gz
+        bcftools view -r 3 $HOME/vcf_files/$sample_id.vcf.gz -o $HOME/vcf_files/${sample_id}_chr3.vcf.gz || { echo "Failed to filter $sample_id.vcf.gz"; exit 1; }
+        
         
         # Delete the original file
         rm $HOME/vcf_files/$sample_id.vcf.gz
 
         # Concatenate the new file with the combined file
-        bcftools concat $HOME/vcf_files/combined_chr3.vcf.gz $HOME/vcf_files/${sample_id}_chr3.vcf.gz -o $HOME/vcf_files/temp_combined.vcf.gz -O z
+        bcftools concat $HOME/vcf_files/combined_chr3.vcf.gz $HOME/vcf_files/${sample_id}_chr3.vcf.gz -o $HOME/vcf_files/temp_combined.vcf.gz -O z || { echo "Failed to concatenate $sample_id.vcf.gz"; exit 1; }
 
         # Move the temporary file to the original file for the next iteration
         mv $HOME/vcf_files/temp_combined.vcf.gz $HOME/vcf_files/combined_chr3.vcf.gz
@@ -61,6 +62,6 @@ if [[ -f "$csv_file" ]]; then
     # Clean up the temporary file
     rm temp_head.csv
 else
-    echo "The metadata CSV file does not exist."
+    echo "The metadata CSV file does not exist or is empty."
     exit 1
 fi
