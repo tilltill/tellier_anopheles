aaa4fce92be91f6c15f5b3d391bc2ab2ae20dbf6
Till Ohlendorf <till.ohlendorf@tum.de>
2024-03-28 13:58:47 +0100

    added comment

--- vcf_download/download_sample_set_metadata.sh
+++ vcf_download/download_sample_set_metadata.sh
@@ -8,8 +8,8 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10G
 
-sample_set_names=("AG1000G-CD", "AG1000G-GM-A", "AG1000G-GM-B", "AG1000G-GM-C", "AG1000G-KE",
-                  "AG1000G-TZ", "1264-VO-CD-WATSENGA-VMF00161", "1264-VO-CD-WATSENGA-VMF00164", "1272-VO-GM-OPONDO-VMF00160",
+sample_set_names=("AG1000G-CD" "AG1000G-GM-A" "AG1000G-GM-B" "AG1000G-GM-C" "AG1000G-KE"
+                  "AG1000G-TZ" "1264-VO-CD-WATSENGA-VMF00161" "1264-VO-CD-WATSENGA-VMF00164" "1272-VO-GM-OPONDO-VMF00160"
                   "1246-VO-TZ-KABULA-VMF00197")
 
 for sample_set in "${sample_set_names[@]}"; do
@@ -21,3 +21,7 @@ for sample_set in "${sample_set_names[@]}"; do
     wget -P $HOME/vo_agam_release/v3/metadata/general/$sample_set/ "https://storage.googleapis.com/vo_agam_release/v3/metadata/general/$sample_set/samples.meta.csv"
 
 done
+
+
+
+#### SCRIPT DOES NOT WORK BECAUSE LINK TO DOWNLOAD DOES NOT WORK FOR EVERY FILE ####
