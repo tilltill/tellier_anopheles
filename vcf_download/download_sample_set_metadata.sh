0e480e4c88b3ba72fecdac37f099933665dd470f
Till Ohlendorf <till.ohlendorf@tum.de>
2024-03-28 11:41:52 +0100

    added sbatch commands

--- vcf_download/download_sample_set_metadata.sh
+++ vcf_download/download_sample_set_metadata.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=downlaod_sample_set_metadata
+#SBATCH --output=downlaod_sample_set_metadata.out
+#SBATCH --error=downlaod_sample_set_metadata.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
+
 sample_set_names=("AG1000G-CD", "AG1000G-GM-A", "AG1000G-GM-B", "AG1000G-GM-C", "AG1000G-KE",
                   "AG1000G-TZ", "1264-VO-CD-WATSENGA-VMF00161", "1264-VO-CD-WATSENGA-VMF00164", "1272-VO-GM-OPONDO-VMF00160",
                   "1246-VO-TZ-KABULA-VMF00197")
