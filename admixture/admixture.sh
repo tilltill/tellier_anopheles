4e12922d0bcfd98c29d5aa7b064145c4718bc33c
Till Ohlendorf <till.ohlendorf@tum.de>
2024-04-11 09:58:32 +0200

    run admixture

---
+++ admixture/admixture.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+cd ~/output/admixture/DRC_AG1000G-CD
+
+# Paths
+bed_file=$HOME/vcf_files/AG1000G-CD_combined_chr3R3L.bed
+output=$HOME/output/admixture/DRC_AG1000G-CD
+
+for K in {2..10}; do
+    admixture $bed_file $K --cv | tee log{$K}.out; done
