f0499b6cf2bfc6f2dcf90cf682f56b254adbfb40
Till Ohlendorf <till.ohlendorf@tum.de>
2024-04-16 10:47:07 +0200

    code to preprocess and analyse vcf batches

---
+++ preprocessing/analyse_vcf_file.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Path to the VCF file
+input_file = 
+filename=$(basename -- "$input_file")
+filename="${filename%.*}"
+
+# Make directory for the subset
+mkdir -p ~/vcf_files/${filename}_analysis
+mkdir -p ~/vcf_files/${filename}_analysis/vcftools
+
+# Change to the directory
+cd ~/vcf_files/${filename}_analysis
+
+# Create random subsample of the VCF file
+bcftools view $input_file | vcfrandomsample -r 0.012 > ${filename}_subset.vcf
+
+# Compress and index subset
+bcftools view ${filename}_subset.vcf -Oz -o ${filename}_subset.vcf.gz
+bcftools index ${filename}_subset.vcf.gz
+
+# Name of the subset VCF file
+SUBSET_VCF=~/vcf_files/${filename}_analysis/${filename}_subset.vcf.gz
+OUT=~/vcf_files/${filename}_analysis/vcftools/${filename}_subset
+
+# Calculate allele frequencies
+vcftools --gzvcf $SUBSET_VCF --freq2 --out $OUT --max-alleles 2
+
+# Calculate mean depth per individual
+vcftools --gzvcf $SUBSET_VCF --depth --out $OUT
+
+# Calculate mean depth per site
+vcftools --gzvcf $SUBSET_VCF --site-mean-depth --out $OUT
+
+# Calculate site quality
+vcftools --gzvcf $SUBSET_VCF --site-quality --out $OUT
+
+# Calculate missingness per individual
+vcftools --gzvcf $SUBSET_VCF --missing-indv --out $OUT
+
+# Calculate missingness per site
+vcftools --gzvcf $SUBSET_VCF --missing-site --out $OUT
+
+# Calculate heterozygosity and inbreeding coefficient per individual
+vcftools --gzvcf $SUBSET_VCF --het --out $OUT
+
+
---
+++ preprocessing/preprocessing.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+input_file=
+
+# Get filename without extension or path
+filename=$(basename -- "$input_file")
+filename="${filename%.*}"
+
+# Remove indels 
+bcftools view -V indels $input_file -Oz -o ${filename}_preprocessed.vcf.gz
+
+# Index the new file
+bcftools index -f ${filename}_preprocessed.vcf.gz
+
+# Decompose the variants
+bcftools norm -m -any ${filename}_preprocessed.vcf.gz -Oz -o ${filename}_preprocessed.vcf.gz
+
+# Index the new file
+bcftools index -f ${filename}_preprocessed.vcf.gz
+
+# Filter for biallelic SNPs
+bcftools view -m2 -M2 -v snps ${filename}_preprocessed.vcf.gz -Oz -o ${filename}_preprocessed.vcf.gz
+
+# Index the new file
+bcftools index -f ${filename}_preprocessed.vcf.gz
+
+# Add missingness Info
+bcftools +fill-tags $input_file -Oz -o ${filename}_preprocessed.vcf.gz -- -t F_MISSING
+
+# Index the new file
+bcftools index -f ${filename}_preprocessed.vcf.gz
+
+# Remove variants with F_MISSING > 0.25
+bcftools view ${filename}_preprocessed.vcf.gz -i 'F_MISSING < 0.3' -Oz -o ${filename}_preprocessed.vcf.gz
+
+# Index the new file
+bcftools index -f ${filename}_preprocessed.vcf.gz
+
+# Remove variants with read depth < 10 or > 100
+bcftools filter -i 'AVG(DP)>10 && AVG(DP)<100' ${filename}_preprocessed.vcf.gz -Oz -o ${filename}_preprocessed.vcf.gz
+
+# Index the new file
+bcftools index -f ${filename}_preprocessed.vcf.gz
+
+
