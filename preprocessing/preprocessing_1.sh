b5ac21bd693dee2cadf2effabb363053a950a371
Till Ohlendorf <till.ohlendorf@tum.de>
2024-04-17 09:36:00 +0200

    changed parameters

--- preprocessing/preprocessing_1.sh
+++ preprocessing/preprocessing_1.sh
@@ -11,9 +11,9 @@ bcftools view -V indels $input_file | \
 bcftools norm -m -any | \
 bcftools view -m2 -M2 -v snps | \
 bcftools +fill-tags -- -t F_MISSING | \
-bcftools view -i 'F_MISSING < 0.3' | \
+bcftools view -i 'F_MISSING < 0.25' | \
 bcftools filter -i 'AVG(DP)>10 && AVG(DP)<100' | \
-bcftools filter -i 'AVG(GQ)>20' -Oz -o ${filename}_preprocessed.vcf.gz
+bcftools filter -i 'AVG(GQ)>30' -Oz -o ${filename}_preprocessed.vcf.gz
 
 # Index the final file
 bcftools index -f ${filename}_preprocessed.vcf.gz
