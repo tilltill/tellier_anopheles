71a9c074704f28cb0e9491c3b07f985925e8359f
Till Ohlendorf <till.ohlendorf@tum.de>
2024-05-05 19:27:32 +0200

    new file

--- admixture/admixture.sh
+++ admixture/admixture.sh
@@ -3,7 +3,7 @@
 cd $HOME/output/admixture/DRC_AG1000G-CD
 
 # Paths
-bed_file=$HOME/vcf_files/AG1000G-CD_combined_chr3R3L.bed
+bed_file=$HOME/vcf_files/AG1000G-CD_combined_chr1_2_sitefilter_PASS_trimmed_biallelic.bed
 
 # Run admixture for K = 2 to 10
 for K in {2..10}; do
