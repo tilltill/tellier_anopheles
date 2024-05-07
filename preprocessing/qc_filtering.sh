0ec4b368a727f33ee2c3b3c857bf327d256dc74b
Till Ohlendorf <till.ohlendorf@tum.de>
2024-05-07 16:56:08 +0200

    different names for two output files

--- preprocessing/qc_filtering.sh
+++ preprocessing/qc_filtering.sh
@@ -2,7 +2,9 @@
 
 # Path to in and out VCF files
 VCF_IN=~/vcf_files/all_samples_PASS_trimmed_biallelic.vcf.gz
-VCF_OUT=~/vcf_files/all_samples_QC_noMAC.vcf.gz
+VCF_OUT_MAC=~/vcf_files/all_samples_QC_MAC.vcf.gz
+VCF_OUT_noMAC=~/vcf_files/all_samples_QC_noMAC.vcf.gz
+
 
 # Filter parameters
 MAC=1
@@ -15,10 +17,10 @@ vcftools --gzvcf $VCF_IN \
     --mac $MAC --max-missing $MISS \
     --min-meanDP $MIN_DEPTH --max-meanDP $MAX_DEPTH \
     --minDP $MIN_DEPTH --maxDP $MAX_DEPTH --recode --stdout | gzip -c > \
-    $VCF_OUT
+    $VCF_OUT_MAC
 
 # Filter the VCF file without MAC
 vcftools --gzvcf $VCF_IN \
     --max-missing $MISS --min-meanDP $MIN_DEPTH --max-meanDP $MAX_DEPTH \
     --minDP $MIN_DEPTH --maxDP $MAX_DEPTH --recode --stdout | gzip -c > \
-    $VCF_OUT
+    $VCF_OUT_noMAC
