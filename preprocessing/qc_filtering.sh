a45974fade69deca52fc78c41cf74d2d524c74fd
Till Ohlendorf <till.ohlendorf@tum.de>
2024-05-07 16:54:27 +0200

    absolute paths instead of cd

--- preprocessing/qc_filtering.sh
+++ preprocessing/qc_filtering.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Path to the VCF file and filename
-input_file=~/vcf_files/all_samples_PASS_trimmed_biallelic.vcf.gz
-filename=$(basename "$input_file" .vcf.gz)
+# Path to in and out VCF files
+VCF_IN=~/vcf_files/all_samples_PASS_trimmed_biallelic.vcf.gz
+VCF_OUT=~/vcf_files/all_samples_QC_noMAC.vcf.gz
 
 # Filter parameters
 MAC=1
@@ -11,14 +11,14 @@ MIN_DEPTH=10
 MAX_DEPTH=60
 
 # Filter the VCF file with MAC
-vcftools --gzvcf ${filename}_sitefilter_PASS_trimmed.vcf.gz \
+vcftools --gzvcf $VCF_IN \
     --mac $MAC --max-missing $MISS \
     --min-meanDP $MIN_DEPTH --max-meanDP $MAX_DEPTH \
     --minDP $MIN_DEPTH --maxDP $MAX_DEPTH --recode --stdout | gzip -c > \
-    ${filename}_QC_MAC.vcf.gz
+    $VCF_OUT
 
 # Filter the VCF file without MAC
-vcftools --gzvcf ${filename}_sitefilter_PASS_trimmed.vcf.gz \
+vcftools --gzvcf $VCF_IN \
     --max-missing $MISS --min-meanDP $MIN_DEPTH --max-meanDP $MAX_DEPTH \
     --minDP $MIN_DEPTH --maxDP $MAX_DEPTH --recode --stdout | gzip -c > \
-    ${filename}_QC_noMAC.vcf.gz
+    $VCF_OUT
