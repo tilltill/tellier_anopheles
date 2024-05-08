42fd371c5dcf2a519ef5a0a012a097924897ad5a
Till Ohlendorf <till.ohlendorf@tum.de>
2024-05-08 09:12:40 +0200

    multiple input files and removed old code

--- preprocessing/subset_analysis.sh
+++ preprocessing/subset_analysis.sh
@@ -1,57 +1,52 @@
 #!/bin/bash
 
-# Path to the VCF file
-input_file=~/vcf_files/all_samples_PASS_trimmed_biallelic.vcf.gz
-filename=$(basename "$input_file" .vcf.gz)
+input_files=("~/vcf_files/all_samples_QC_MAC.vcf.gz" "~/vcf_files/all_samples_QC_noMAC.vcf.gz")
 
-# Make directory for the subset
-mkdir -p ~/vcf_files/${filename}_analysis
-mkdir -p ~/vcf_files/${filename}_analysis/vcftools
+for input_file in ${input_files[@]}; do
+    # Path to the VCF file
+    filename=$(basename "$input_file" .vcf.gz)
 
-# Copy the VCF file to the new directory
-cd ~/vcf_files/
-cp $input_file ~/vcf_files/${filename}_analysis/
+    # Make directory for the subset
+    mkdir -p ~/vcf_files/${filename}_analysis
+    mkdir -p ~/vcf_files/${filename}_analysis/vcftools
 
-# Change to the directory
-cd ~/vcf_files/${filename}_analysis
+    # Copy the VCF file to the new directory
+    cd ~/vcf_files/
+    cp $input_file ~/vcf_files/${filename}_analysis/
 
-# Create random subsample of the VCF file
-bcftools view ${filename}.vcf.gz | vcfrandomsample -r 0.012 > ${filename}_subset.vcf
+    # Change to the directory
+    cd ~/vcf_files/${filename}_analysis
 
-# Compress and index subset
-bcftools view ${filename}_subset.vcf -Oz -o ${filename}_subset.vcf.gz
-bcftools index ${filename}_subset.vcf.gz
+    # Create random subsample of the VCF file
+    bcftools view ${filename}.vcf.gz | vcfrandomsample -r 0.012 > ${filename}_subset.vcf
 
-# Remove indels and decompose the variants
-#bcftools norm -m -any | \
-#bcftools view -V indels ${filename}_subset.vcf.gz | \
-#bcftools view -m2 -M2 -v snps -Oz -o ${filename}_subset.vcf.gz
+    # Compress and index subset
+    bcftools view ${filename}_subset.vcf -Oz -o ${filename}_subset.vcf.gz
+    bcftools index ${filename}_subset.vcf.gz
 
-# Index the new file (no indels and decomposed)
-#bcftools index -f ${filename}_subset.vcf.gz
+    # Name of the subset VCF file
+    SUBSET_VCF=~/vcf_files/${filename}_analysis/${filename}_subset.vcf.gz
+    OUT=~/vcf_files/${filename}_analysis/vcftools/${filename}_subset
 
-# Name of the subset VCF file
-SUBSET_VCF=~/vcf_files/${filename}_analysis/${filename}_subset.vcf.gz
-OUT=~/vcf_files/${filename}_analysis/vcftools/${filename}_subset
+    # Calculate allele frequencies
+    vcftools --gzvcf $SUBSET_VCF --freq2 --out $OUT --max-alleles 2
 
-# Calculate allele frequencies
-vcftools --gzvcf $SUBSET_VCF --freq2 --out $OUT --max-alleles 2
+    # Calculate mean depth per individual
+    vcftools --gzvcf $SUBSET_VCF --depth --out $OUT
 
-# Calculate mean depth per individual
-vcftools --gzvcf $SUBSET_VCF --depth --out $OUT
+    # Calculate mean depth per site
+    vcftools --gzvcf $SUBSET_VCF --site-mean-depth --out $OUT
 
-# Calculate mean depth per site
-vcftools --gzvcf $SUBSET_VCF --site-mean-depth --out $OUT
+    # Calculate site quality
+    vcftools --gzvcf $SUBSET_VCF --site-quality --out $OUT
 
-# Calculate site quality
-vcftools --gzvcf $SUBSET_VCF --site-quality --out $OUT
+    # Calculate missingness per individual
+    vcftools --gzvcf $SUBSET_VCF --missing-indv --out $OUT
 
-# Calculate missingness per individual
-vcftools --gzvcf $SUBSET_VCF --missing-indv --out $OUT
+    # Calculate missingness per site
+    vcftools --gzvcf $SUBSET_VCF --missing-site --out $OUT
 
-# Calculate missingness per site
-vcftools --gzvcf $SUBSET_VCF --missing-site --out $OUT
+    # Calculate heterozygosity and inbreeding coefficient per individual
+    vcftools --gzvcf $SUBSET_VCF --het --out $OUT
 
-# Calculate heterozygosity and inbreeding coefficient per individual
-vcftools --gzvcf $SUBSET_VCF --het --out $OUT
-
+done
