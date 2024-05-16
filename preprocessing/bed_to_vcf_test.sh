b6a78fba1c125082a950d7323ed921c14c3dd0b1
Till Ohlendorf <till.ohlendorf@tum.de>
2024-05-16 19:06:32 +0200

    test if plink to vcf conversion is causing only missing values error

---
+++ preprocessing/bed_to_vcf_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+input_files=("$HOME/vcf_files/all_samples_QC_MAC.vcf.gz")
+
+
+# Change to the directory
+cd ~/vcf_files
+
+# Convert the VCF file to PLINK format and back to vcf.gz format to test if the conversion works
+
+for input_file in ${input_files[@]}; do
+    # Path to the VCF file
+    filename=$(basename "$input_file" .vcf.gz)
+
+    # Convert the VCF file to PLINK format
+    plink --vcf ${filename}.vcf.gz --make-bed --out ${filename}_test
+
+    # Convert filtered data back to vcf.gz format
+    plink --bfile ${filename}_test --recode vcf-iid bgz --out ${filename}_test
+
+    # Print the number of variants in the original LD pruned, HWE pruned and LD+HWE pruned VCF files
+    echo "Number of variants in the original VCF file:"
+    bcftools view -H ${filename}.vcf.gz | wc -l
+    
+    echo "Number of variants in the test file:"
+    bcftools view -H ${filename}_LD.vcf.gz | wc -l
+
+done
