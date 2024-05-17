57ee595adc84f947d27cde916121406cab438617
Till Ohlendorf <till.ohlendorf@tum.de>
2024-05-17 11:16:35 +0200

    seven instead of ten k

--- admixture/admixture.sh
+++ admixture/admixture.sh
@@ -6,6 +6,6 @@ cd $HOME/output/admixture/all_samples_QC_MAC
 # Paths
 bed_file=$HOME/vcf_files/all_samples_QC_MAC.bed
 
-# Run admixture for K = 2 to 10
-for K in {2..10}; do
+# Run admixture for K = 2 to 7
+for K in {2..7}; do
     admixture $bed_file $K --cv | tee log{$K}.out; done 
