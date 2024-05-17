870e938170dd9bd2c798e039e201df1167d0e1bd
Till Ohlendorf <till.ohlendorf@tum.de>
2024-05-17 11:14:43 +0200

    run with all_samples_QC_MAC

--- admixture/admixture.sh
+++ admixture/admixture.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
-cd $HOME/output/admixture/all_samples
+mkdir -p $HOME/output/admixture/all_samples_QC_MAC
+cd $HOME/output/admixture/all_samples_QC_MAC
 
 # Paths
-bed_file=$HOME/vcf_files/all_samples_PASS_trimmed_biallelic.bed
+bed_file=$HOME/vcf_files/all_samples_QC_MAC.bed
 
 # Run admixture for K = 2 to 10
 for K in {2..10}; do
